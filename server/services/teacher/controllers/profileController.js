548b845ee5a9224c75e833e1d51c69a7ecc1b5fb
abijith <abijithsurendran005@gmail.com>
2024-07-05 00:05:21 +0530

    certificate upload completed

--- server/services/teacher/controllers/profileController.js
+++ server/services/teacher/controllers/profileController.js
@@ -1,13 +1,17 @@
 import multer from "multer";
 import uploadPdf from "../helpers/certificateUpload.js";
+import requestModel from "../models/categoryRequestModeldel.js";
 
 export const uploadCertificates = async (req, res) => {
   try {
-    const files = req.files.map((file) => ({
+    const { category } = req.body;
+    const certificates = req.files.map((file) => ({
       url: file.location,
       key: file.key,
+      verified: false,
     }));
-
+    console.log(certificates);
+    await requestModel.create({userId:'131',category:category,certificates:certificates})
     res
       .status(200)
       .json({ success: true, message: "Certficates uploaded successfully" });
---
+++ server/services/teacher/models/categoryRequestModeldel.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const requestSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      // type:String,
+      required: [true, "User id required"],
+    },
+    category: {
+      type: String,
+      required: [true, "Category required"],
+    },
+    certificates: [
+      {
+        url: {
+          type: String,
+          required: [true, "Certificate URL required"],
+        },
+        key: {
+          type: String,
+          required: [true, "Certificate key is required"],
+        },
+        verified: {
+          type: Boolean,
+          default: false,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      enum: ["accepted", "rejected", "pending"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
+);
+
+const requestModel = mongoose.model("Requests", requestSchema);
+
+export default requestModel;
